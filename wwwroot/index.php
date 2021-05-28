b26abe5843892b2c0f5b8db177f3bd72692b8aa1
Michael Chen <cwchen123@gmail.com>
2021-05-28 19:29:19 +0800

    Update index.php

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . "/../vendor/autoload.php";
 
-require_once __DIR__ . '/../setting.php';
+require_once __DIR__ . "/../setting.php";
 require_once __DIR__ . "/../" . LIBRARY_DIRECTORY . "/utils.php";
 
 # Remove it later.
