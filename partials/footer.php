b0bf2965854a90ab5daf95233a8f86e3485594a6
Michael Chen <cwchen123@gmail.com>
2021-05-28 14:38:22 +0800

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -3,4 +3,10 @@
     integrity="sha512-ui7r9OLlcZJsUe16m7uoiyX9wBdbLds3zB5WCtlV+Sp+2U/wFFhGbp8Q4BfPbFEb0iBG07LsCvHDVySlVmgJAw=="
     crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
+<!-- highlight.js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.2/highlight.min.js"
+    integrity="sha512-s+tOYYcC3Jybgr9mVsdAxsRYlGNq4mlAurOrfNuGMQ/SCofNPu92tjE7YRZCsdEtWL1yGkqk15fU/ark206YTg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>hljs.highlightAll();</script>
+
 <!-- Add your own scripts here. -->
