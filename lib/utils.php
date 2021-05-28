287ffa8dfeaa7c668c4643419459a064b5e12631
Michael Chen <cwchen123@gmail.com>
2021-05-28 15:54:27 +0800

    Update utils.php

--- lib/utils.php
+++ lib/utils.php
@@ -32,3 +32,37 @@ function parsePage($page) {
 
     return $result;
 }
+
+# TODO: Refactor it later.
+define("CONTENT_DIRECTORY", "content");
+
+# TODO: Test the code.
+function fetchContent($arr) {
+    $result = "";
+
+    if (0 == count($arr)) {
+        # Pass.   
+    }
+    else if (1 == count($arr)) {
+        $html_path = __DIR__ . "/../" 
+            . CONTENT_DIRECTORY . "/"
+            . $arr[0] . ".html";
+        $markdown_path = __DIR__ . "/../" 
+            . CONTENT_DIRECTORY . "/"
+            . $arr[0] . ".md";
+
+        # Here we just set higher priority for HTML pages.
+        # We may change it later.
+        if (file_exists($html_path)) {
+            $result = file_get_contents($html_path);
+        }
+        else if (file_exists($markdown_path)) {
+            $result = file_get_contents($markdown_path);
+        }
+    }
+    else {
+        # TODO: Implement it later.
+    }
+
+    return $result;
+}
