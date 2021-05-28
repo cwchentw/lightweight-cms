91c18d350de9638fb52aa4700d12dffaece3cfa9
Michael Chen <cwchen.tw@outlook.com>
2021-05-28 23:55:40 +0000

    Refactor some code

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -23,18 +23,24 @@ if (false != strpos($loc, "..")) {
     goto render;
 }
 
-$post = fetchPage($loc);
-
-# Fallback to default title.
-if (404 == $post["status"]) {
-    $post["title"] = "Page Not Found";
-    $post["content"] = "The post doesn't exist on the website";
-}
-
 render:
-    # Currently, we use a superglobal variable to pass data.
-    # Change it later.
-    $GLOBALS["post"] = $post;
+    if (isset($post) && 200 != $post["status"]) {
+        # Currently, we use a superglobal variable to pass data.
+        $GLOBALS["post"] = $post;
 
-    # Change it later.
-    require __DIR__ . "/../" . LAYOUT_DIRECTORY . "/post.php";
+        require __DIR__ . "/../" . LAYOUT_DIRECTORY . "/post.php";
+    }
+    else {
+        $post = fetchPage($loc);
+
+        # HTTP 404.
+        if (404 == $post["status"]) {
+            $post["title"] = "Page Not Found";
+            $post["content"] = "The post doesn't exist on the website";
+        }
+
+        # Currently, we use a superglobal variable to pass data.
+        $GLOBALS["post"] = $post;
+
+        require __DIR__ . "/../" . LAYOUT_DIRECTORY . "/post.php";
+    }
