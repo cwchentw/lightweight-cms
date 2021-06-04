380f691397e40fce92495869ef5018151676c135
Michael Chen <cwchen.tw@outlook.com>
2021-06-04 22:44:10 +0000

    Minor fix

--- scripts/50x.php
+++ scripts/50x.php
@@ -6,6 +6,7 @@
 require_once __DIR__ . "/../setting.php";
 require_once __DIR__ . "/../" . LIBRARY_DIRECTORY . "/autoload.php";
 
+
 $post = array();
 
 $post[MDCMS_POST_TITLE] = "Internal Server Error";
