41b40b16ae664d4f81f09fce0295aed6a3139fca
Michael Chen <cwchen.tw@outlook.com>
2021-06-04 11:25:55 +0000

    Update setting.php

--- setting.php
+++ setting.php
@@ -2,39 +2,55 @@
 # The project configuration file. Keep its name *as is*.
 
 
-# The base URL of the site.
+# The information of a site.
+
+# The base URL of a site without a trailing slash.
 define("SITE_BASE_URL", "https://example.com");
-# Set the name of the website.
+# The full name of a site.
 define("SITE_NAME", "mdcms");
-# Set the short name of the website.
+# The short name of a site.
 define("SITE_SHORT_NAME", "mdcms");
-# Set the description of the website.
-define("SITE_DESCRIPTION", "A Markdown-based Content Management System Powered by PHP");
-# Set the author of the website.
-# Currently, we only support websites of single author.
+# A concise description of a site.
+define("SITE_DESCRIPTION", "A Markdown-based Dynamic Website Generator Powered by PHP");
+# The principal author of a site.
+#
+# If there are multiple authors in a site,
+#  set the author of a specific post in its metadata.
 define("SITE_AUTHOR", "mdcms");
-# Set the language of rendered HTML pages.
-# Currently, we only support websites of single language.
+# The language of rendered HTML pages.
+#
+# mdcms is not designed for a multi-language site.
+#  You may require two or more sets of layouts and
+#  some modifications on the router of mdcms for such site.
 define("SITE_LANGUAGE", "en-US");
-# Set the text of the index page of breadcrumbs.
+# The text of the home page on breadcrumbs.
 define("SITE_BREADCRUMB_HOME", "Home");
+
+
+# The parameters of a site.
 
 # The upper limit of word count of excerpts.
 # It should be less than the upper limit of a tweet of Twitter.
 define("EXCERPT_THRESHOLD", 240);
 
+
 # These flags will switch on or off optional features.
 
-# Add supports to PWA (progressive web application).
-# You need to make a manifest.json with our ./scripts/manifest script manually.
+# Enable the supports to PWA (progressive web application).
 define("ENABLE_PWA", false);
-# Add support to hightlight.js
-# Currently, the theme of hightlight.js is fixed.
+# Enable the support to hightlight.js
+# Currently, the theme of hightlight.js is hard coded.
 define("ENABLE_CODE_HIGHTLIGHT", false);
 # Block search engine bots on sections.
+#
+# Such action is optional but recommended for SEO.
 define("BLOCK_BOT", false);
 # Scan application directory.
+#
+# By default, mdcms only scans sections and posts
+#  in the content directory.
 define("SCAN_APPLICATION_DIRECTORY", false);
+
 
 # Mostly, you don't require to modify anything below.
 # If either is modified, modify the project accordingly as well.
@@ -53,15 +69,15 @@ define("SITEMAP_LAYOUT", "sitemap.php");
 # The index files used by sections.
 define("SECTION_INDEX", "_index.md");
 
-# The directory of posts.
+# The directory of sections and posts.
 define("CONTENT_DIRECTORY", "content");
 # The directory of the layouts.
 define("LAYOUT_DIRECTORY", "layout");
 # The directory of the partials.
 define("PARTIALS_DIRECTORY", "partials");
-# The directory of the private libraries.
+# The directory of the internal libraries of mdcms.
 define("LIBRARY_DIRECTORY", "lib");
-# The directory of the application.
+# The directory of the web application.
 define("APPLICATION_DIRECTORY", "wwwroot");
 # The directory of public files.
 define("PUBLIC_DIRECTORY", "public");
