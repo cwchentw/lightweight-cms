5d372c275f3ca36ef93f6c66e3d02dfa8d4aeb82
Michael Chen <cwchen123@gmail.com>
2021-06-07 05:15:50 +0800

    Update utils.php

--- src/utils.php
+++ src/utils.php
@@ -16,3 +16,49 @@ function isSection($page)
 
     return is_dir($path);
 }
+
+# TODO: Check it later.
+function xCopy($src, $dst)
+{
+    if (!mkdir($dst, 0755, true)) {
+        # We may find a better exception for this event.
+        # TODO: Refactor it later.
+        throw new Exception("Unable to create directory: " . $dst . "\n");
+    }
+
+    $dir = opendir($src);
+    if (!dir) {
+        # We may find a better exception for this event.
+        # TODO: Refactor it later.
+        throw new Exception("Unable to open directory: " . $src . "\n");
+    }
+
+    while(false !== ( $file = readdir($dir)) ) {
+        if (( $file != '.' ) && ( $file != '..' )) {
+            if ( is_dir($src . '/' . $file) ) {
+                try {
+                    xCopy($src . '/' . $file, $dst . '/' . $file);
+                }
+                catch (Exception $e) {
+                    echo $e->getMessage();
+                }
+                finally {
+                    # Release system resources.
+                    closedir($dir);
+                }
+            }
+            else {
+                if (!copy($src . '/' . $file, $dst . '/' . $file)) {
+                    # Release system resources.
+                    closedir($dir);
+  
+                    # We may find a better exception for this event.
+                    # TODO: Refactor it later.
+                    throw new Exception("Unable to create file: " . $dst . "/" . $file . "\n");
+                }
+            }
+        }
+    }
+ 
+    closedir($dir);
+}
