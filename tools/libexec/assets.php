1c7e5ac5137248ffc968234ef67a4443b92b3340
Michael Chen <cwchen123@gmail.com>
2021-06-08 09:14:46 +0800

    Update assets.php

--- tools/libexec/assets.php
+++ tools/libexec/assets.php
@@ -1,14 +1,21 @@
 <?php
 # An asset loader.
 #
+
+# Get the absolute path of a local mdcms.
 $rootDirectory = __DIR__ . "/../..";
+
+# Load global setting.
 require_once $rootDirectory . "/setting.php";
+
+# Load required libraries.
 require_once $rootDirectory . "/" . LIBRARY_DIRECTORY . "/autoload.php";
 require_once $rootDirectory . "/" . PLUGIN_DIRECTORY . "/autoload.php";
 require_once $rootDirectory . "/" . THEME_DIRECTORY . "/" . SITE_THEME . "/autoload.php";
 
+
 try {
-    $publicDirectory = __DIR__ . "/../../public";
+    $publicDirectory = $rootDirectory . "/public";
     loadAssets($publicDirectory);
 }
 catch (Exception $e) {
