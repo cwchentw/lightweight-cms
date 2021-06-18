d462017388c570016e5dcb5023f81cac0d9453b5
Michelle Chen <4949545+cwchentw@users.noreply.github.com>
2021-06-18 20:03:25 +0800

    Update copyright.php

--- themes/default/partials/copyright.php
+++ themes/default/partials/copyright.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col text-center info-title">
     <?php if (!is_null(SITE_COPYRIGHT) && "" != SITE_COPYRIGHT): ?>
-        <?php echo SITE_COPYRIGHT ?>
+        <?php echo SITE_COPYRIGHT; ?>
     <?php else: ?>
         Powered by <a href="https://github.com/cwchentw/mdcms" target="_blank" rel="noopener nofollow">mdcms</a>
     <?php endif; ?>
