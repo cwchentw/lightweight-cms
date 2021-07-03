866b8d5d97a1b4592d5ff929ef56b562fa664a91
Michelle Chen <cwchen.tw@outlook.com>
2021-07-03 12:51:25 +0800

    Publish a mdcms site to a PaaS on Windows

---
+++ tools/bin/publish.bat
@@ -0,0 +1,70 @@
+@echo off
+rem Publish a mdmcs site to a PaaS.
+
+
+rem Check whether PHP is available on the system.
+php --version >nul || (
+    echo No PHP on the system >&2
+    exit /b 1
+)
+
+set address=%1
+if "" == "%address%" (
+    set address=localhost:5000
+)
+
+rem Get working directory of current batch script.
+set cwd=%~dp0
+rem Get path to executables.
+set bin=%cwd%..\bin
+rem Get path to library.
+set lib=%cwd%..\lib
+rem Get path to PHP scripts.
+set libexec=%cwd%..\libexec
+
+rem Generate site settings if it doesn't exist.
+if not exist %lib%\settings.bat (
+    call %bin%\init.bat || (
+        exit /b %ERRORLEVEL%
+    )
+)
+
+rem Load site settings.
+call %lib%\settings.bat
+
+rem Create a 50x.html
+call %bin%\50x.bat || (
+    exit /b %ERRORLEVEL%
+)
+
+rem Create a manifest.json
+call %bin%\manifest.bat || (
+    exit /b %ERRORLEVEL%
+)
+
+rem Create a sitemap.xml
+call %bin%\sitemap.bat || (
+    exit /b %ERRORLEVEL%
+)
+
+rem Load theme assets.
+call %bin%\assets.bat || (
+    exit /b %ERRORLEVEL%
+)
+
+rem Load personal assets.
+call %bin%\site-assets.bat || (
+    exit /b %ERRORLEVEL%
+)
+
+rem Copy static files.
+xcopy /s /y %static% %public% || (
+    echo Unable to copy static files to the public directory >&2
+    exit /b 1
+)
+
+rem Copy router of mdcms.
+copy /y %www%\index.php %public% || (
+    echo Unable to copy router of mdmcs to public directory >&2
+    exit /b 1
+)
