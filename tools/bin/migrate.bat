745592f3cb645462a5540ce2bc01f995beb29dbe
Michelle Chen <cwchen.tw@outlook.com>
2021-07-10 06:56:51 +0800

    Update some code

--- tools/bin/migrate.bat
+++ tools/bin/migrate.bat
@@ -75,7 +75,11 @@ sed -i "s/cwchentw\/mdcms//" %socialMediaConfig:\=/% || (
     echo /b 1
 )
 
-rem TODO: Update redirecting rules.
+set parametersConfig=%root%\config\parameters.php
+sed -i "/define(""REDIRECT_LIST""/ { N; N; N; N; N; s/define(""REDIRECT_LIST"", .*);/define(""REDIRECT_LIST"", []);/; }" %parametersConfig:\=/% || (
+    echo Unable to modify parameters.php >&2
+    echo /b 1
+)
 
 set optionalFeaturesConfig=%root%\config\optionalFeatures.php
 sed -i "s/UA-105146581-5//" %optionalFeaturesConfig:\=/% || (
