2188989c30fd2e4b5ff73f5fc62ceaf1ae541d38
Michelle Chen <cwchen.tw@outlook.com>
2022-07-25 10:26:55 +0800

    Update the navbar; update some text

--- content/reference/_index.md
+++ content/reference/_index.md
@@ -1,6 +1,6 @@
 ---
-title: References
+title: Reference
 weight: 1
 ---
 
-The references of various parts of Lightweight CMS. They don't intend for tutorials for beginners. If you are still unfamiliar with Lightweight CMS, read our introductory articles first.
+The reference of Lightweight CMS. It doesn't intend as the tutorial for beginners. If you are still unfamiliar with Lightweight CMS, read our introductory articles first.
--- themes/default/partials/navbar.php
+++ themes/default/partials/navbar.php
@@ -12,65 +12,21 @@ function hasSocialMedia()
 }
 ?>
 
-<nav class="navbar navbar-expand-lg navbar-dark">
+<nav class="site-navbar">
     <div class="container">
-        <a class="navbar-brand" href="<?php echo SITE_PREFIX . "/"; ?>"><?php echo SITE_SHORT_NAME; ?></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarSiteInfo" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Site Info
-                  </a>
-                  <ul class="dropdown-menu" aria-labelledby="navbarSiteInfo">
-                    <li><a class="dropdown-item" href="<?php echo SITE_PREFIX . "/about/"; ?>">About the Site</a></li>
-                    <li><a class="dropdown-item" href="<?php echo SITE_PREFIX . "/terms-and-conditions/"; ?>">Terms and Conditions</a></li>
-                    <li><a class="dropdown-item" href="<?php echo SITE_PREFIX . "/privacy-policy/"; ?>">Privacy Policy</a></li>
-                  </ul>
-                </li>
-                <?php if (hasSocialMedia()): ?>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarMedia" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Social Media
-                  </a>
-                  <ul class="dropdown-menu" aria-labelledby="navbarMedia">
-                    <?php if (!is_null(FACEBOOK) && "" != FACEBOOK): ?>
-                    <!-- The link to Facebook (personal account or fan page). -->
-                    <li><a class="dropdown-item"
-                        href="https://facebook.com/<?php echo FACEBOOK; ?>"
-                        target="_blank" rel="noopener nofollow">Facebook</a>
-                    </li>
-                    <?php endif; ?>
-
-                    <?php if (!is_null(FACEBOOK_GROUP) && "" != FACEBOOK_GROUP): ?>
-                    <!-- The link to Facebook group. -->
-                    <li><a class="dropdown-item"
-                        href="https://facebook.com/groups/<?php echo FACEBOOK_GROUP; ?>"
-                        target="_blank" rel="noopener nofollow">Facebook Group</a>
-                    </li>
-                    <?php endif; ?>
-
-                    <?php if (!is_null(TWITTER) && "" != TWITTER): ?>
-                    <!-- The link to Twitter. -->
-                    <li><a class="dropdown-item"
-                        href="https://twitter.com/<?php echo TWITTER; ?>"
-                        target="_blank" rel="noopener nofollow">Twitter</a>
-                    </li>
-                    <?php endif; ?>
-
-                    <?php if (!is_null(GITHUB) && "" != GITHUB): ?>
-                    <!-- The link to GitHub. -->
-                    <li><a class="dropdown-item"
-                        href="https://github.com/<?php echo GITHUB; ?>"
-                        target="_blank" rel="noopener nofollow">GitHub</a>
-                    </li>
-                    <?php endif; ?>
-                  </ul>
-                </li>
-                <?php endif; ?>
-            </ul>
-        </div>
+        <a href="<?php echo SITE_PREFIX . "/"; ?>">
+            <?php echo SITE_SHORT_NAME; ?>
+        </a>
+        <ul>
+            <li>
+                <a href="<?php echo SITE_PREFIX . "/reference/" ?>">Reference</a>
+            </li>
+            <li>
+                <a href="<?php echo SITE_PREFIX . "/howto/" ?>">HOWTOs</a>
+            </li>
+            <li>
+                <a href="<?php echo SITE_PREFIX . "/tip/" ?>">Tips</a>
+            </li>
+        </ul>
     </div>
 </nav>
