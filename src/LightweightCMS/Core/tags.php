0ca527cf73cfded3fc93d3fff96046e200b9ae18
Michelle Chen <cwchen.tw@outlook.com>
2022-10-26 08:31:42 +0800

    Refactor some code

--- src/LightweightCMS/Core/tags.php
+++ src/LightweightCMS/Core/tags.php
@@ -6,34 +6,35 @@ namespace LightweightCMS\Core;
 function getTags ()
 {
     $sep = DIRECTORY_SEPARATOR;
+    $rootDirectory = __DIR__ . $sep . ".." . $sep . ".." . $sep . "..";
+    # Load the site settings.
+    require_once $rootDirectory . $sep . "setting.php";
 
-    $rootDirectory = __DIR__ . "{$sep}..{$sep}..{$sep}..";
-    # Load global setting.
-    require_once $rootDirectory . "{$sep}setting.php";
+    $result = array();
 
     $dataDirectory = $rootDirectory . $sep . PUBLIC_DIRECTORY . $sep . "data";
     $json = file_get_contents($dataDirectory . $sep . "tags.json");
 
-    $result = array();
+    if ($json) {
+        $tags = json_decode($json, JSON_UNESCAPED_UNICODE);
 
-    $tags = json_decode($json, JSON_UNESCAPED_UNICODE);
+        foreach ($tags as $tag => $paths) {
+            $link = array();
 
-    foreach ($tags as $tag => $paths) {
-        $link = array();
+            $link[LIGHTWEIGHT_CMS_POST_META] = array();
+            $link[LIGHTWEIGHT_CMS_POST_TITLE] = $tag;
+            $link[LIGHTWEIGHT_CMS_POST_META]["description"] = $tag . " related articles";
+            $link[LIGHTWEIGHT_CMS_LINK_PATH] =
+                "/tags/"
+                  # Trick to replace ` ` into `%20` in a URL string.
+                . preg_replace("/_/", "%20", urlencode(preg_replace("/ /", "_", $tag)))
+                . "/";
 
-        $link[LIGHTWEIGHT_CMS_POST_META] = array();
-        $link[LIGHTWEIGHT_CMS_POST_TITLE] = $tag;
-        $link[LIGHTWEIGHT_CMS_POST_META]["description"] = $tag . " related articles";
-        $link[LIGHTWEIGHT_CMS_LINK_PATH] =
-            "/tags/"
-              # Trick to replace ` ` into `%20` in a URL string.
-            . preg_replace("/_/", "%20", urlencode(preg_replace("/ /", "_", $tag)))
-            . "/";
+            array_push($result, $link);
+        }
 
-        array_push($result, $link);
+        usort($result, $GLOBALS[SORT_POST_CALLBACK]);
     }
-
-    usort($result, $GLOBALS[SORT_POST_CALLBACK]);
 
     return $result;
 }
@@ -69,27 +70,29 @@ function tagPosts ($uri)
     preg_match("/^\/tags\/(.+)\/$/", $uri, $matches);
     $tag = urldecode($matches[1]);
 
-    $rootDirectory = __DIR__ . "{$sep}..{$sep}..{$sep}..";
+    $rootDirectory = __DIR__ . $sep . ".." . $sep . ".." . $sep . "..";
     $dataDirectory = $rootDirectory . $sep . PUBLIC_DIRECTORY . $sep . "data";
     $json = file_get_contents($dataDirectory . $sep . "tags.json");
 
     $result = array();
 
-    $tags = json_decode($json);
+    if ($json) {
+        $tags = json_decode($json);
 
-    foreach ($tags as $t => $p) {
-        if ($t !== $tag)
-            continue;
+        foreach ($tags as $t => $p) {
+            if ($t !== $tag)
+                continue;
 
-        foreach ($p as $path) {
-            $post = readPost($path);
-            $post[LIGHTWEIGHT_CMS_LINK_PATH] = $path;
+            foreach ($p as $path) {
+                $post = readPost($path);
+                $post[LIGHTWEIGHT_CMS_LINK_PATH] = $path;
 
-            array_push($result, $post);
+                array_push($result, $post);
+            }
         }
-    }
 
-    usort($result, $GLOBALS[SORT_POST_CALLBACK]);
+        usort($result, $GLOBALS[SORT_POST_CALLBACK]);
+    }
 
     return $result;
 }
@@ -121,31 +124,25 @@ function tagPageBreadcrumb ($tag)
     # Create a breadcrumb dynamically.
     $breadcrumb = array();
 
-    {
-        $home = array();
+    $home = array();
 
-        $home[LIGHTWEIGHT_CMS_LINK_PATH] = SITE_PREFIX . "/";
-        $home[LIGHTWEIGHT_CMS_LINK_TITLE] = BREADCRUMB_HOME;
+    $home[LIGHTWEIGHT_CMS_LINK_PATH] = SITE_PREFIX . "/";
+    $home[LIGHTWEIGHT_CMS_LINK_TITLE] = BREADCRUMB_HOME;
 
-        array_push($breadcrumb, $home);
-    }
+    array_push($breadcrumb, $home);
 
-    {
-        $tags = array();
+    $tags = array();
 
-        $tags[LIGHTWEIGHT_CMS_LINK_PATH] = SITE_PREFIX . "/tags/";
-        $tags[LIGHTWEIGHT_CMS_LINK_TITLE] = "Tags";
+    $tags[LIGHTWEIGHT_CMS_LINK_PATH] = SITE_PREFIX . "/tags/";
+    $tags[LIGHTWEIGHT_CMS_LINK_TITLE] = "Tags";
 
-        array_push($breadcrumb, $tags);
-    }
+    array_push($breadcrumb, $tags);
 
-    {
-        $tagPage = array();
+    $tagPage = array();
 
-        $tagPage[LIGHTWEIGHT_CMS_LINK_TITLE] = $tag;
+    $tagPage[LIGHTWEIGHT_CMS_LINK_TITLE] = $tag;
 
-        array_push($breadcrumb, $tagPage);
-    }
+    array_push($breadcrumb, $tagPage);
 
-return $breadcrumb;
+    return $breadcrumb;
 }
